6c494bf5e4cc56dbc23c0b8d3165da0f47bd456d
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-04-24 12:01:37 +0330

    install Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/views/**/*.html'
+    ],
+    theme: {
+        screens: {
+            'xs': '360px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
+        extend: {},
+    },
+    plugins: [
+        require('tailwindcss-debug-screens'),
+    ]
+}
