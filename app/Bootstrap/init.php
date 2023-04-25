36dad35f3233c4c5643c44b6ed894c4581ac46d8
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-04-25 14:11:08 +0330

    implement DotEnv Adapter class

---
+++ app/Bootstrap/init.php
@@ -0,0 +1,20 @@
+<?php
+
+use app\Core\Adapter\DotEnvAdapter;
+
+define('BASE_APP_PATH', __DIR__ . '../../../');
+
+require_once realpath(BASE_APP_PATH . 'vendor/autoload.php');
+
+try {
+    (new DotEnvAdapter(BASE_APP_PATH))->requiredElement();
+} catch (Exception $e) {
+    // todo: DotEnvAdapter exception
+    die($e->getMessage());
+}
+
+require_once BASE_APP_PATH . 'app/Helpers/constants.php';
+
+ini_set('display_errors', $_ENV['DISPLAY_ERRORS']);
+ini_set('display_startup_errors', $_ENV['DISPLAY_STARTUP_ERRORS']);
+error_reporting($_ENV['ERROR_REPORTING']);
